d2460f366b964aeb5e4f8a8b9df4ebb57c2aab7f
= <moeezbutt977@gmail.com>
2025-01-22 00:32:10 +0500

    removed visiting new page in every test case with sessions management

--- cypress/e2e/test/testToDownloadBookPdf.cy.js
+++ cypress/e2e/test/testToDownloadBookPdf.cy.js
@@ -5,6 +5,8 @@ import TheHobbitBookPdfPage from "../../support/pageObject/theHobbitBookPdfPage"
 import TheHobbitDetailPage from "../../support/pageObject/theHobbitDetailPage";
 import TheHobbitSearchPage from "../../support/pageObject/theHobbitSearchPage";
 
+let lastVisitedUrl;
+
 describe("PDF Drive Page Tests", () => {
   const pdfDrivePage = new PDFDrivePage();
   const searchedPage = new TheHobbitSearchPage();
@@ -12,11 +14,31 @@ describe("PDF Drive Page Tests", () => {
   const bookIframe = new TheHobbitBookIframe();
   const pdfPage = new TheHobbitBookPdfPage();
 
+  beforeEach(() => {
+    cy.session("user-session", () => {
+      pdfDrivePage.visit();
+    });
+    cy.restoreLocalStorage();
+    if (lastVisitedUrl) {
+      cy.visit(lastVisitedUrl);
+    }
+  });
+
   before(() => {
     pdfDrivePage.visit();
     pdfDrivePage.enterSearchQuery("The Hobbit");
     pdfDrivePage.clickSearchButton();
     cy.contains("h1", "The Hobbit Books").should("exist");
+    cy.url().then((url) => {
+      lastVisitedUrl = url;
+    });
+  });
+
+  afterEach(() => {
+    cy.saveLocalStorage();
+    cy.url().then((url) => {
+      lastVisitedUrl = url;
+    });
   });
 
   it("Should apply filters on the search results page", () => {
@@ -32,18 +54,16 @@ describe("PDF Drive Page Tests", () => {
   });
 
   it("Should verify the URL, title, pages length and click on preview button", () => {
-    detailPage.visit();
     detailPage.assertUrl("https://www.pdfdrive.com/the-hobbit-e18760817.html");
     detailPage.assertBookTitle("The Hobbit");
     detailPage.assertPagesLength("24");
     detailPage.clickPreviewButton();
   });
 
-  it("Should interect with Iframe and scroll to bottom", () => {
-    bookIframe.visit();
+  it("Should interact with Iframe and scroll to bottom", () => {
     bookIframe.clickPreviewButton();
     bookIframe.assertModalIsOpen();
-    bookIframe.handleLoadingProcess()
+    bookIframe.handleLoadingProcess();
     bookIframe.scrollToBottomOfContainer();
     bookIframe.extractTextBetweenMarkers(
       "about this guide’s authors",
@@ -54,7 +74,6 @@ describe("PDF Drive Page Tests", () => {
   });
 
   it("Should wait for the progress bar and download the PDF", () => {
-    pdfPage.visit();
     pdfPage.waitForProgressBar();
     pdfPage.assertProgressBarAboveHundred();
     pdfPage.downloadPdf();
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,3 +25,17 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 import "cypress-iframe";
 import "cypress-wait-until";
+
+let LOCAL_STORAGE_MEMORY = {};
+
+Cypress.Commands.add("saveLocalStorage", () => {
+  Object.keys(localStorage).forEach((key) => {
+    LOCAL_STORAGE_MEMORY[key] = localStorage[key];
+  });
+});
+
+Cypress.Commands.add("restoreLocalStorage", () => {
+  Object.keys(LOCAL_STORAGE_MEMORY).forEach((key) => {
+    localStorage.setItem(key, LOCAL_STORAGE_MEMORY[key]);
+  });
+});
